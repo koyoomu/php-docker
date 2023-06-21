23cfce74809b70a1cb036d72e86967918755eadb
koyoomu <koyomi0503@gmail.com>
2023-06-21 15:52:15 +0900

    task2-2アップします

--- html/task2-2.php
+++ html/task2-2.php
@@ -8,6 +8,33 @@
   <title>PHP練習</title>
 </head>
 <body>
-  
+  <table>
+    <?php
+    $title = array('商品','価格','税込価格');
+    $pencil = array('鉛筆','100円','110円');
+    $erase = array('消しゴム','200円','220円');
+    $ruler = array('鉛筆','300円','330円');
+    ?>
+    <tr>
+      <th><?php echo $title[0] ?></th>
+      <th><?php echo $title[1] ?></th>
+      <th><?php echo $title[2] ?></th>
+    </tr>
+    <tr>
+      <td><?php echo $pencil[0]?></td>
+      <td><?php echo $pencil[1]?></td>
+      <td><?php echo $pencil[2]?></td>
+    </tr>
+    <tr>
+      <td><?php echo $erase[0]?></td>
+      <td><?php echo $erase[1]?></td>
+      <td><?php echo $erase[2]?></td>
+    </tr>
+    <tr>
+      <td><?php echo $ruler[0]?></td>
+      <td><?php echo $ruler[1]?></td>
+      <td><?php echo $ruler[2]?></td>
+    </tr>
+  </table>
 </body>
 </html>
